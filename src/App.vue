346ea7f9ec8885b3c566f22b3c83bf477353cc80
salimov <salimov@infomatika.ru>
2024-01-26 08:28:39 +0400

    feat: Доработка вёрстки

--- src/App.vue
+++ src/App.vue
@@ -15,36 +15,38 @@
         </div>
         <div class="main__row">
           <div class="row__items">
-            <div class="item">
-              <div class="item__title">12 сент 2023</div>
-              <div class="item__info">
-                <div class="item__distance">
-                  <div class="item__distance-text">3 лунные орбиты</div>
-                  <div class="item__distance-arr">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="129" height="6" viewBox="0 0 129 6" fill="none">
-                      <path d="M0 3L5 5.88675L5 0.113249L0 3ZM129 3.00001L124 0.113259L124 5.88676L129 3.00001ZM4.5 3.5L124.5 3.50001L124.5 2.50001L4.5 2.5L4.5 3.5Z" fill="white" fill-opacity="0.5"/>
-                    </svg>
+            <template v-for="nearEarthObject in nearEarthObjects[1]" :key="nearEarthObject.neo_reference_id">
+              <div class="item">
+                <div class="item__title">{{ nearEarthObjects[0] }} 12 сент 2023</div>
+                <div class="item__info">
+                  <div class="item__distance">
+                    <div class="item__distance-text">{{ Math.round(nearEarthObject.close_approach_data[0].miss_distance.lunar) }} лунные орбиты</div>
+                    <div class="item__distance-arr">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="129" height="6" viewBox="0 0 129 6" fill="none">
+                        <path d="M0 3L5 5.88675L5 0.113249L0 3ZM129 3.00001L124 0.113259L124 5.88676L129 3.00001ZM4.5 3.5L124.5 3.50001L124.5 2.50001L4.5 2.5L4.5 3.5Z" fill="white" fill-opacity="0.5"/>
+                      </svg>
+                    </div>
+                  </div>
+                  <div class="item__img">
+                    <img src="./assets/pngegg 2.png">
+                  </div>
+                  <div class="item__right">
+                    <div class="item__right-title">2024FQ</div>
+                    <div class="item__right-size">Ø 85 м</div>
                   </div>
                 </div>
-                <div class="item__img">
-                  <img src="./assets/pngegg 2.png">
-                </div>
-                <div class="item__right">
-                  <div class="item__right-title">2024FQ</div>
-                  <div class="item__right-size">Ø 85 м</div>
-                </div>
-              </div>
-              <div class="item__bottom">
-                <div class="item__bottom-button">заказать</div>
-                <div class="item__bottom-warning">
-                  <svg width="13px" height="13px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--fxemoji" preserveAspectRatio="xMidYMid meet">
-                    <path fill="#FFB636" d="M12.51 470.379L234.371 16.008c6.439-13.187 25.17-13.363 31.855-.299l232.51 454.371c6.064 11.849-2.542 25.92-15.853 25.92H28.512c-13.164 0-21.778-13.791-16.002-25.621z"></path>
-                    <path fill="#2B3B47" d="M284.332 173L272.15 336.498c-.911 12.233-11.567 21.411-23.8 20.499c-11.116-.828-19.706-9.707-20.499-20.499L215.668 173c-1.413-18.961 12.813-35.478 31.774-36.89s35.478 12.813 36.89 31.774c.124 1.662.109 3.5 0 5.116zM250 391.873c-17.432 0-31.564 14.131-31.564 31.564C218.436 440.869 232.568 455 250 455s31.564-14.131 31.564-31.564c0-17.432-14.132-31.563-31.564-31.563z"></path>
-                  </svg>
-                  Опасен
+                <div class="item__bottom">
+                  <div class="item__bottom-button">заказать</div>
+                  <div class="item__bottom-warning">
+                    <svg width="13px" height="13px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--fxemoji" preserveAspectRatio="xMidYMid meet">
+                      <path fill="#FFB636" d="M12.51 470.379L234.371 16.008c6.439-13.187 25.17-13.363 31.855-.299l232.51 454.371c6.064 11.849-2.542 25.92-15.853 25.92H28.512c-13.164 0-21.778-13.791-16.002-25.621z"></path>
+                      <path fill="#2B3B47" d="M284.332 173L272.15 336.498c-.911 12.233-11.567 21.411-23.8 20.499c-11.116-.828-19.706-9.707-20.499-20.499L215.668 173c-1.413-18.961 12.813-35.478 31.774-36.89s35.478 12.813 36.89 31.774c.124 1.662.109 3.5 0 5.116zM250 391.873c-17.432 0-31.564 14.131-31.564 31.564C218.436 440.869 232.568 455 250 455s31.564-14.131 31.564-31.564c0-17.432-14.132-31.563-31.564-31.563z"></path>
+                    </svg>
+                    Опасен
+                  </div>
                 </div>
               </div>
-            </div>
+            </template>
           </div>
         </div>
         <div class="basket">
@@ -65,23 +67,39 @@
 </template>
 
 <script>
-// import axios from 'axios'
+import axios from 'axios'
 export default {
+  data () {
+    return {
+      dataSpase: {},
+      nearEarthObjects: []
+    }
+  },
+
   mounted () {
-    this.test()
+    this.getApp()
   },
 
   methods: {
-    test () {
-      // axios.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2024-01-01&end_date=2024-01-01&api_key=lxFg4mPZkaDYsADeWLV9KdiZQds7T9A8zVih7Yuk').then(response => console.log(response))
+    async getApp () {
+      const url = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2024-01-01&end_date=2024-01-01&api_key=lxFg4mPZkaDYsADeWLV9KdiZQds7T9A8zVih7Yuk'
+      await axios.get(url).then(response => {
+        this.dataSpase = response.data
+        console.log(this.dataSpase)
+        const x = { 20: [0, 1, 2, 2] }
+        console.log(x)
+        console.log(x[0])
+        console.log(1, Object.entries(response.data.near_earth_objects)[0])
+        this.nearEarthObjects = Object.entries(response.data.near_earth_objects)[0]
+        console.log(this.nearEarthObjects)
+      })
     }
   }
 }
 </script>
 
 <style lang="scss">
-@import 'assets/fonts.css';
-@import 'scss/reset.scss';
+@import './scss/reset.scss';
 
 :root {
   --Black: #000;
@@ -107,9 +125,7 @@ export default {
   min-height: 100vh;
   background-color: var(--Black);
   padding: 16px 16px 96px 16px;
-
 }
-
 .content{
   position: relative;
   z-index: 1;
@@ -164,8 +180,8 @@ export default {
 
     font-family: Helvetica;
     font-size: 24px;
-    font-style: normal;
     font-weight: 700;
+    font-style: normal;
     line-height: 32px; /* 133.333% */
   }
 
@@ -234,10 +250,10 @@ export default {
   right: 0;
   padding: 16px;
   background-color: #232526;
-  display: flex;
   justify-content: space-between;
   align-items: center;
   color: #FFF;
+  display: flex;
   font-family: Helvetica;
   font-size: 16px;
   font-style: normal;
